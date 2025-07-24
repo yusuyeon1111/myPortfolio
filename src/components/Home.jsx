3258044286d886057768af60abe74736d0f0f7c4
yusueon1111 <yusuyeon443@gmail.com>
2025-07-24 21:51:21 +0900

    반응형 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,29 +7,12 @@ function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
         <div className='home-container'>
-          <div className={`hambuger ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-
-          <div className='item-container'>
-            {isOpen && (
-              <div className='menuOpen'>
-                <p>About Me</p>
-                <p>Stack</p>
-                <p>Project</p>
-                <p>Contact</p>
-              </div>
-            )}
-
             <div className='info-container'>
               <p id='firstP'>Welcome to my</p>
               <img src={folderBack} id='folderBack'/>
               <p id='portfolio'>Portfolio</p>
               <AnimateComponent/>
             </div>
-          </div>
         </div>
 
   )
